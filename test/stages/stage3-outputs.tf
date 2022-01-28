99ef7c564c98f27146e4ecf6e59f2fe2ce39de7c
Jyoti Rani <jyotirani10@in.ibm.com>
2022-01-28 16:57:14 +0530

    updated stage2-eventstreams
    Signed-off-by: Jyoti Rani <jyotirani10@in.ibm.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -1,17 +1,17 @@
 
 resource null_resource write_outputs {
   provisioner "local-exec" {
-    command = "echo \"$${OUTPUT}\" > gitops-output.json"
+    //  command = "echo \"$${OUTPUT}\" > eventstreams-output.json"
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.gitops_module.name
-        branch      = module.gitops_module.branch
-        namespace   = module.gitops_module.namespace
-        server_name = module.gitops_module.server_name
-        layer       = module.gitops_module.layer
-        layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-        type        = module.gitops_module.type
+        name   = module.eventstreams_module.name
+        branch = module.eventstreams_module.branch
+        //  namespace   = module.eventstreams_module.namespace
+        server_name = module.eventstreams_module.server_name
+        layer       = module.eventstreams_module.layer
+        layer_dir   = module.eventstreams_module.layer == "infrastructure" ? "1-infrastructure" : (module.eventstreams_module.layer == "services" ? "2-services" : "3-applications")
+        type        = module.eventstreams_module.type
       })
     }
   }
