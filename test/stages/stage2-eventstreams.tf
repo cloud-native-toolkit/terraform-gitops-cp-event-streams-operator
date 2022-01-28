1d3802669e31e9d2ca0991e74bd066b221b9c2f6
Jyoti Rani <jyotirani10@in.ibm.com>
2022-01-28 16:53:22 +0530

    updated stage2-eventstreams
    Signed-off-by: Jyoti Rani <jyotirani10@in.ibm.com>

--- test/stages/stage2-eventstreams.tf
+++ test/stages/stage2-eventstreams.tf
@@ -1,9 +1,10 @@
-module "gitops_module" {
+module "eventstreams_module" {
   source = "./module"
 
-  gitops_config = module.gitops.gitops_config
+  gitops_config   = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
-  kubeseal_cert = module.gitops.sealed_secrets_cert
+  server_name     = module.gitops.server_name
+  catalog         = module.cp_catalogs.catalog_ibmoperators
+  channel         = module.cp4i-dependencies.mq.channel
+  //  kubeseal_cert   = module.gitops.sealed_secrets_cert
 }
