930152e7b0e4b279e62a27f0113089d955605cbd
Jyoti Rani <jyotirani10@in.ibm.com>
2022-01-28 17:35:27 +0530

    changed mq occurrences to ES
    Signed-off-by: Jyoti Rani <jyotirani10@in.ibm.com>

--- test/stages/stage2-eventstreams.tf
+++ test/stages/stage2-eventstreams.tf
@@ -6,6 +6,6 @@ module "eventstreams_module" {
   server_name     = module.gitops.server_name
   catalog         = module.cp_catalogs.catalog_ibmoperators
   //  channel         = module.cp4i-dependencies.eventstreams.channel
-  channel = "v2.2-eus"
+  channel = "v2.3"
   //  kubeseal_cert   = module.gitops.sealed_secrets_cert
 }
