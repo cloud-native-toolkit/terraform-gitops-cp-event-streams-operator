8617177eaada7bc70de22688615dd8f55051c929
Jyoti Rani <jyotirani10@in.ibm.com>
2022-01-31 14:37:22 +0530

    changed ES channel
    Signed-off-by: Jyoti Rani <jyotirani10@in.ibm.com>

--- test/stages/stage2-eventstreams.tf
+++ test/stages/stage2-eventstreams.tf
@@ -6,6 +6,6 @@ module "eventstreams_module" {
   server_name     = module.gitops.server_name
   catalog         = module.cp_catalogs.catalog_ibmoperators
   //  channel         = module.cp4i-dependencies.eventstreams.channel
-  channel = "v2.3"
+  channel = "v2.5.0"
   //  kubeseal_cert   = module.gitops.sealed_secrets_cert
 }
