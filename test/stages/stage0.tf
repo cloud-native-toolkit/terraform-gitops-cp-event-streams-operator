4f60bf6fd8bd8e39b4acee35977b7e7f81755d8a
Sean Sundberg <seansund@us.ibm.com>
2022-02-09 10:56:38 -0600

    Adds -n to echo command

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -11,6 +11,6 @@ resource "null_resource" "setup_clis" {
   ]
   provisioner "local-exec" {
     #command = “echo -n ‘${module.clis.bin_dir}’ > .bin_dir”
-    command = "echo ${module.setup_clis.bin_dir} > .bin_dir"
+    command = "echo -n '${module.setup_clis.bin_dir}' > .bin_dir"
   }
 }
